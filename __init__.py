a4e41d80a758aa2e4315c42e50d6b8c004c024a4
Ole Schuett <ole.schuett@empa.ch>
2017-03-27 11:42:07 +0200

    new package format

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Some package meta-data
+
+def get_app_description():
+    return "A k-path finder and visualizer"
+
+def get_app_displayname():
+    return "SeeK-path"
+    
+def get_app_notebooks():
+    return {'SeeK-path': 'seekpath.ipynb'}
+    
+#EOF
